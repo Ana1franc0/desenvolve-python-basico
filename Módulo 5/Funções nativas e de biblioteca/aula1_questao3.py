c55102f34a9233ca279015189d32808fc04ca9e8
Ana1franc0 <162919622+Ana1franc0@users.noreply.github.com>
2024-06-16 18:38:15 -0300

    Update aula1_questao3.py

---
+++ Módulo 5/Funções nativas e de biblioteca/aula1_questao3.py
@@ -0,0 +1,13 @@
+import random
+
+n = random.randint(1, 10)
+
+while True: 
+    palpite = int(input("Adivinhe o número entre 1 e 10: "))
+    if palpite > n:
+        print("Muito alto, tente novamente!")
+    elif palpite < n:
+         print("Muito baixo, tente novamente!")
+    else:
+        print("Correto!")
+        break
